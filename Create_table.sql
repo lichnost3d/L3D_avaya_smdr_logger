35277b35e3b24fc975ee75dbf9f364cec0d982b5
lichnost3d <43925505+lichnost3d@users.noreply.github.com>
2018-10-27 21:19:54 +0300

    Update Create_table.sql

--- Create_table.sql
+++ Create_table.sql
@@ -36,6 +36,46 @@ CREATE TABLE `smdr` (
 	`ExternalTargeterId` VARCHAR(50) NULL DEFAULT NULL,
 	`ExternalTargetedNumber` VARCHAR(50) NULL DEFAULT NULL
 )
+
+CREATE TABLE `cdr` (
+	`id` INT(10) NOT NULL AUTO_INCREMENT,
+	`calldate` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+	`clid` VARCHAR(80) NOT NULL DEFAULT '',
+	`src` VARCHAR(80) NOT NULL DEFAULT '' COMMENT 'Кто звонил',
+	`dst` VARCHAR(80) NOT NULL DEFAULT '' COMMENT 'Куда звонил',
+	`realdst` VARCHAR(80) NOT NULL DEFAULT '',
+	`dcontext` VARCHAR(80) NOT NULL DEFAULT '',
+	`channel` VARCHAR(80) NOT NULL DEFAULT '',
+	`dstchannel` VARCHAR(80) NOT NULL DEFAULT '',
+	`lastapp` VARCHAR(80) NOT NULL DEFAULT '',
+	`lastdata` VARCHAR(80) NOT NULL DEFAULT '',
+	`start` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Начало звонка',
+	`answer` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Начало разговора',
+	`end` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Конец звонка',
+	`duration` INT(11) NOT NULL DEFAULT '0' COMMENT 'Продолжительность',
+	`billsec` INT(11) NOT NULL DEFAULT '0',
+	`disposition` VARCHAR(45) NOT NULL DEFAULT '',
+	`amaflags` INT(11) NOT NULL DEFAULT '0',
+	`remoteip` VARCHAR(60) NOT NULL DEFAULT '',
+	`accountcode` VARCHAR(20) NOT NULL DEFAULT '',
+	`peeraccount` VARCHAR(20) NOT NULL DEFAULT '',
+	`uniqueid` VARCHAR(32) NOT NULL DEFAULT '',
+	`userfield` VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'Комментарий',
+	`did` VARCHAR(50) NOT NULL DEFAULT '',
+	`linkedid` VARCHAR(32) NOT NULL DEFAULT '',
+	`sequence` INT(11) NOT NULL DEFAULT '0',
+	`filename` VARCHAR(255) NULL DEFAULT 'none' COMMENT 'Путь к записи',
+	PRIMARY KEY (`id`),
+	INDEX `calldate` (`calldate`),
+	INDEX `src` (`src`),
+	INDEX `dst` (`dst`),
+	INDEX `accountcode` (`accountcode`),
+	INDEX `uniqueid` (`uniqueid`),
+	INDEX `dcontext` (`dcontext`),
+	INDEX `clid` (`clid`),
+	INDEX `did` (`did`)
+)
+
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 ;
